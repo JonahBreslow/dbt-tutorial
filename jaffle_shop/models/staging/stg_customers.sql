a58ffd6c2dd3585f13c038cbbe133bb109f8d3e7
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-01-31 09:10:37 -0800

    final dbt presentation:

---
+++ jaffle_shop/models/staging/stg_customers.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        tags='stage',
+        materialized='ephemeral'
+    )
+}}
+
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from `dbt-tutorial`.jaffle_shop.customers
